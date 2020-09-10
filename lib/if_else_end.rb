08df5e8963ede794957c923923b71f674f84d971
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-10 16:25:09 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,9 @@
 # Write your solution here
 def if_else_end
  current_time = Time.now
+ current_time = current_time.to_i
   
-  if current_time.to_i  % 0 == 0 
+  if current_time % 0 == 0 
     puts "Even!"
   else
     puts "Odd!"
